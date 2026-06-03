e394c959a71332dec742f0359ca979f519191c28
Zdenko55 <zdenkogaraj@gymko.edu.rs>
2026-06-03 13:33:45 +0200

    Add files via upload

---
+++ Andjela_Biblioteka_B7/autor.php
@@ -0,0 +1,16 @@
+<?php
+$title = "Biblioteka | O autoru";
+$active = "autor";
+require "./components/header.php";
+?>
+
+<main>
+    <section>
+        <h2>O autoru</h2>
+        <p>Ime i prezime: Zdenko Garaj</p>
+        <p>Email: zdenko.garaj2007@gmail.com</p>
+        <p>Naziv aplikacije: Biblioteka</p>
+    </section>
+</main>
+
+<?php require "./components/footer.php"; ?>
---
+++ Andjela_RezervacijaAutobuskihKarata_B12/autor.php
@@ -0,0 +1,16 @@
+<?php
+$title = "Rezervacija autobuskih karata | O autoru";
+$active = "autor";
+require "./components/header.php";
+?>
+
+<main>
+    <section>
+        <h2>O autoru</h2>
+        <p>Ime i prezime: Zdenko Garaj</p>
+        <p>Email: zdenko.garaj2007@gmail.com</p>
+        <p>Naziv aplikacije: Rezervacija autobuskih karata</p>
+    </section>
+</main>
+
+<?php require "./components/footer.php"; ?>
